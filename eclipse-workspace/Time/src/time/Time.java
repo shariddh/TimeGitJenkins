d9dd4c1bb38632060e0d9812565711e02c2b8cb7
Riddhi <shariddh@sheridancollege.ca>
2022-03-10 13:28:37 -0500

    Github commit

--- eclipse-workspace/Time/src/time/Time.java
+++ eclipse-workspace/Time/src/time/Time.java
@@ -23,7 +23,7 @@ public class Time
 {
 	public static void main(String[] args) 
 	{
-		int totalSeconds = getTotalSeconds("10:10:10");
+		int totalSeconds = getTotalSeconds("Github 10:10:10");
 		System.out.println("Total Seconds = " + totalSeconds);
 //		try
 //		{
